9402a06d4295bf995b72fbecd148eff02566de9b
James Kirk <JamesKirk95@hotmail.com>
2018-01-11 17:50:39 +0000

    Can read in from sensor but requires tuning

--- Control/Control.ino
+++ Control/Control.ino
@@ -1,31 +1,103 @@
 #include <ZumoMotors.h>
+#include <ZumoReflectanceSensorArray.h>
+#include <QTRSensors.h>
+#include <Pushbutton.h>
+
 ZumoMotors motors;
+ZumoReflectanceSensorArray reflectanceSensors;
+Pushbutton button(ZUMO_BUTTON);
+
+const int speed = 50;
+const int turnSpeed = 200;
+const int turnDuration = 500; //0.5 seconds
+
+const int corridorThreshold = 140;
 
 void setup() {
-  // put your setup code here, to run once:
+  // Initialise the reflectance module
+  reflectanceSensors.init();
+
+  // Delay 3 seconds
+  delay(3000);
+
+  // LED to alert calibration mode is active
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+
+  for(int i = 0; i < 80; i++){
+    if ((i > 10 && i <= 30) || (i > 50 && i <= 70)) {
+      motors.setSpeeds(-(turnSpeed), turnSpeed);
+    } else {
+      motors.setSpeeds(turnSpeed, -(turnSpeed));
+    }
+    reflectanceSensors.calibrate();
+    delay(20);
+  }
+
+  // Stop Zumo and turn off LED now calibration mode is no longer active
+  motors.setSpeeds(0, 0);
+  digitalWrite(13, LOW);
+
+  button.waitForButton();
+
+  // Begin communication
   Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  unsigned int sensors[6];
+//
+//  int position = reflectanceSensors.readLine(sensors);
+  reflectanceSensors.read(sensors);
+
+  for (byte i = 0; i < 6; i++) {
+    Serial.print(sensors[i]);
+    Serial.print(' ');
+  }
+
+  Serial.println("");
+
+  if (sensors[0] > corridorThreshold){
+    motors.setSpeeds(-(speed), -(speed));
+    delay(turnDuration);
+    motors.setSpeeds(turnSpeed, -(turnSpeed));
+    delay(turnDuration);
+    motors.setSpeeds(speed, speed);
+  } else if (sensors[5] > corridorThreshold) {
+    motors.setSpeeds(-(speed), -(speed));
+    delay(turnDuration);
+    motors.setSpeeds(-(turnSpeed), turnSpeed);
+    delay(turnDuration);
+    motors.setSpeeds(speed, speed);
+    } 
+    //else {
+//    motors.setSpeeds(speed, speed);
+//  }
+
+  // Read in user value for the current command
   char val = Serial.read();
 
+  // Forwards
   if(val == 'w') {
-    motors.setSpeeds(100, 100);
+    motors.setSpeeds(speed, speed);
   }
 
+  // Backwards
   if(val == 'x') {
-    motors.setSpeeds(-100, -100);
+    motors.setSpeeds(-(speed), -(speed));
   }
 
+  // Right
   if(val == 'd') {
-    motors.setSpeeds(100, 0);
-  }
+    motors.setSpeeds(turnSpeed, 0);
+  } 
 
+  // Left
   if(val ==  'a') {
-    motors.setSpeeds(0, 100);
+    motors.setSpeeds(0, turnSpeed);
   }
 
+  // Stop
   if(val == 's') {
     motors.setSpeeds(0, 0);
   }
